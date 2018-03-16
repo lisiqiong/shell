e62d73dff8b094bbcdb3b16acdb79099411aa200
lisiqiong <peterlee20100427@yeah.net>
2018-03-16 09:14:41 +0800

    shell输入参数进行拼接

---
+++ shell基础/helloWorld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#除了第一个是用来宣告shell之外，其它都是批注
+#program
+#this program show "hello world" in you screen
+#history
+#2018/03/15 lisiqiong
+echo -e "hello world!\a\n"
+exit 0
---
+++ shell基础/readP.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Program
+# user inputs his first name and last name.Program shows his full name
+#History
+#2018/03/16 lisiqiong 
+read -p "please input your first name:" firstname #提示使用者输入
+read -p "please input your last name:" lastname #提示使用者输入
+echo -e "\nYour full name is: $firstname $lastname" #结果由屏幕输出
